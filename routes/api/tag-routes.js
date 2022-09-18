5203da05cd8ba1669b34fc52732f8a225b624dc6
Ginger Owen <gingerowen0421@gmail.com>
2022-09-18 11:40:23 -0600

    worked on tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,30 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  // find all tags and associated Product data
+  try {
+    const tagData = await Tag.findAll({ include: [ Product ]});
+    if (!tagData) {
+      res.status(404).json( {message: "Tag not found!"});
+      return;
+    } //end of if statement
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id` associated Product data
+  try {
+    const tagData = await Tag.findByPk({ include: [ Product ]});
+    if (!tagData) {
+      res.status(404).json({message: "Tag not found!"});
+    }
+  } catch (error) {
+    
+  }
 });
 
 router.post('/', (req, res) => {
